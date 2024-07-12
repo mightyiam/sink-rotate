8d802bb5c70d7e9e0f7c2f80edc4011df7457e42
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-12 10:28:12 +0700

    style: rename some test items

--- src/tests.rs
+++ src/tests.rs
@@ -1,16 +1,16 @@
 use crate::Dump;
 
 #[test]
-fn rotate_usb_to_builtin() {
-    const DUMP: &str = include_str!("../fixtures/usb-to-builtin.json");
+fn a() {
+    const DUMP: &str = include_str!("../fixtures/48,_62_.json");
     let dump = Dump::from_json(DUMP);
     let next = dump.next_audio_sink_id();
     assert_eq!(next.0, 48);
 }
 
 #[test]
-fn rotate_builtin_to_usb() {
-    const DUMP: &str = include_str!("../fixtures/builtin-to-usb.json");
+fn b() {
+    const DUMP: &str = include_str!("../fixtures/_48_,62.json");
     let dump = Dump::from_json(DUMP);
     let next = dump.next_audio_sink_id();
     assert_eq!(next.0, 62);
@@ -19,7 +19,7 @@ fn rotate_builtin_to_usb() {
 #[test]
 #[should_panic(expected = "audio sinks not two")]
 fn one_sink_available() {
-    const DUMP: &str = include_str!("../fixtures/one-sink-available.json");
+    const DUMP: &str = include_str!("../fixtures/_48_.json");
     let dump = Dump::from_json(DUMP);
     dump.next_audio_sink_id();
 }
