45ba0bf9a694d2ac30b26b4a10e89da51ffc7624
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-24 16:19:08 +0700

    chore: sort an attrset

--- fmt.nix
+++ fmt.nix
@@ -6,8 +6,8 @@
     projectRootFile = "flake.nix";
     programs = {
       alejandra.enable = true;
-      rustfmt.enable = true;
       prettier.enable = true;
+      rustfmt.enable = true;
     };
     settings.global.excludes = [
       "fixtures/*"
