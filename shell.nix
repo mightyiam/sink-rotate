3de4cd77132b16a134e639b149792d12cf4da05d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-25 10:18:14 +0700

    chore: add rust-analyzer to dev shell

--- shell.nix
+++ shell.nix
@@ -6,7 +6,10 @@
     { pkgs, config, ... }:
     {
       make-shells.default = {
-        packages = [ pkgs.gcc ];
+        packages = [
+          pkgs.gcc
+          pkgs.rust-analyzer
+        ];
         inputsFrom = [ config.packages.default ];
       };
     };
