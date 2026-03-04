3a745843715b67623e074b03c1b7de32571a7d94
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-03-04 07:22:43 +0700

    chore(flake): remove darwin systems

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    systems.url = "github:nix-systems/default-linux";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,12 +15,12 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
       imports = [
         ./fmt.nix
         ./package.nix
         ./release
         ./shell.nix
+        ./systems.nix
       ];
     };
 }
